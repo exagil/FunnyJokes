cd589436f549be71c232f65304f819bdc77c0758
Chirag <me@chi6rag.net>
2016-06-12 15:20:35 +0530

    Adding free and paid flavors

--- app/src/free/java/com/udacity/gradle/builditbigger/WelcomeActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/WelcomeActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/WelcomeActivity.java
@@ -0,0 +1,81 @@
+package com.udacity.gradle.builditbigger;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.udacity.gradle.builditbigger.joke.Joke;
+import com.udacity.gradle.builditbigger.joke.JokePresenter;
+import com.udacity.gradle.builditbigger.joke.JokeService;
+import com.udacity.gradle.builditbigger.joke.JokeView;
+
+import net.chiragaggarwal.jokedisplay.JokeDisplayActivity;
+import net.chiragaggarwal.jokesrepository.Jokes;
+
+import javax.inject.Inject;
+
+public class WelcomeActivity extends ActionBarActivity implements JokeView {
+    @Inject
+    public JokeService jokeService;
+    private JokePresenter jokePresenter;
+    private View viewWelcome;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((FunnyJokesApp) getApplication()).getFunnyJokesDeps().inject(this);
+        progressDialog = new ProgressDialog(this);
+        jokePresenter = new JokePresenter(this, jokeService);
+        viewWelcome = findViewById(R.id.layout_welcome);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void loadJoke(View view) {
+        jokePresenter.loadRandomJoke();
+    }
+
+    @Override
+    public void onJokeLoaded(Joke joke) {
+        Intent jokeDisplayIntent = new Intent(this, JokeDisplayActivity.class);
+        jokeDisplayIntent.putExtra(Jokes.TAG, joke.description);
+        startActivity(jokeDisplayIntent);
+    }
+
+    @Override
+    public void onJokeLoadFailed() {
+        Snackbar.make(viewWelcome, R.string.joke_load_error, Snackbar.LENGTH_SHORT);
+    }
+
+    @Override
+    public void showLoader() {
+        progressDialog.setMessage("Loading Joke");
+        progressDialog.show();
+    }
+
+    @Override
+    public void hideLoader() {
+        progressDialog.dismiss();
+    }
+}
